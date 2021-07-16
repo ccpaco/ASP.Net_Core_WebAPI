7db64fe0a1a1b5b64873d0b2a7cb6ebad6db6d5a
Christian Champaco <chrischampaco@gmail.com>
2021-07-16 10:24:40 -0800

    First push

---
+++ BooksApi/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+//entity model as class Book
+namespace BooksApi.Models
+{
+    public class Book
+    {
+        [BsonId] // labels this as primary key
+        [BsonRepresentation(BsonType.ObjectId)] // mongo accepts string parameters and converts to object for us
+        public string Id { get; set; }
+
+        [BsonElement("Name")] // represents property name in MongoDB collection
+        public string BookName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Author { get; set; }
+
+    }
+}
---
+++ BooksApi/Models/BookstoreDatabaseSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+//this class is used to store appsettings.json's properties
+namespace BooksApi.Models
+{
+    public class BookstoreDatabaseSettings : IBookstoreDatabaseSettings
+    {
+        public string BooksCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IBookstoreDatabaseSettings
+    {
+        string BooksCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+
+}
