54855cbd63f9ff6ac0f6530f0e85c5c89a1a1fb7
Christian Champaco <chrischampaco@gmail.com>
2021-07-16 20:21:38 -0800

    Finished tutorial, error with launchSettings or mongodb port. Successful build, 0 code errors.

--- BooksApi/Controllers/BooksController.cs
+++ BooksApi/Controllers/BooksController.cs
@@ -4,6 +4,12 @@ using BooksApi.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
+// Uses the BookService class to perform CRUD operations.
+// Contains action methods to support GET, POST, PUT, and DELETE HTTP requests.
+// Calls CreatedAtRoute in the Create action method to return an HTTP 201 response. Status code
+// 201 is the standard response for an HTTP POST method that creates a new resource on the server.
+// CreatedAtRoute also adds a Location header to the response. The Location header specifies the
+// URI of the newly created book.
 
 namespace BooksApi.Controllers
 {
